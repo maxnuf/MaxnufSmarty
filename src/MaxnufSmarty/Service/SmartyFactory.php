8e7f302951a6d6e4c4d87dd015082d5f73042d10
nielsj <drsdeurknop@gmail.com>
2013-04-12 23:14:48 +0200

    provide functioncompilerwrapper and modifiercompilerwrapper to smarty add logic to make things work

--- src/MaxnufSmarty/Service/SmartyFactory.php
+++ src/MaxnufSmarty/Service/SmartyFactory.php
@@ -8,6 +8,9 @@ use MaxnufSmarty\Smarty\MaxnufSmarty;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use MaxnufSmarty\Handler\PluginHandler;
+use MaxnufSmarty\Handler\PluginWrapper;
+use MaxnufSmarty\Compiler\FunctionCompiler;
+use MaxnufSmarty\Compiler\ModifierCompiler;
 
 class SmartyFactory implements FactoryInterface
 {
@@ -22,11 +25,19 @@ class SmartyFactory implements FactoryInterface
 //        $resolver = $serviceLocator->get('ViewResolver'); 
 //        $loader->setFallbackResolver($resolver);
 
-        $smarty = new MaxnufSmarty($smartyConfig);
+        $functionCompiler = new FunctionCompiler($manager);
+        $modifierCompiler = new ModifierCompiler($manager);
+
+        $smarty = new MaxnufSmarty($smartyConfig, $functionCompiler, $modifierCompiler);
         $smarty->setTemplateDir($config['view_manager']['template_path_stack']);
         
         $smarty->addPluginsDir($plugins);
+        $smarty->addPluginsDir(__DIR__ . '/../Smarty/SysPlugins');
         
+        $wrapper = new PluginWrapper($manager);
+        $smarty->registerObject('zf', $wrapper, array(), false);
+        
+        $smarty->registerPlugin(MaxnufSmarty::PLUGIN_MODIFIERCOMPILER, 'openTag', array($modifierCompiler, 'openTag'));
 		$smarty->registerPlugin(MaxnufSmarty::PLUGIN_COMPILER, 'formCloseTag', array('Zend\Form\View\Helper\Form', 'closeTag'));
 		
         $handler = new PluginHandler($manager);
